d009c972e7bf3c96d7a10f2a2bf21a49f267bea4
ntepluhina <ntepluhina@gitlab.com>
2020-01-05 16:46:42 +0200

    Removed unused styles

--- docs/.vuepress/components/intro-2.vue
+++ docs/.vuepress/components/intro-2.vue
@@ -15,20 +15,3 @@
     },
   }
 </script>
-
-<style scoped>
-  .demo {
-    border: 1px solid #eee;
-    border-radius: 2px;
-    padding: 25px 35px;
-    margin-top: 1em;
-    margin-bottom: 40px;
-    user-select: none;
-    overflow-x: auto;
-  }
-
-  span {
-    margin-bottom: 0;
-    margin-top: 0;
-  }
-</style>
--- docs/.vuepress/components/intro-3.vue
+++ docs/.vuepress/components/intro-3.vue
@@ -13,15 +13,3 @@
     },
   }
 </script>
-
-<style scoped>
-  .demo {
-    border: 1px solid #eee;
-    border-radius: 2px;
-    padding: 25px 35px;
-    margin-top: 1em;
-    margin-bottom: 40px;
-    user-select: none;
-    overflow-x: auto;
-  }
-</style>
