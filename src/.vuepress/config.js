4f5a8e671a5cb6e2d671c0d1e61ff99f7ade30bd
Ben Hong <ben@bencodezen.io>
2020-04-10 09:33:10 -0400

    feature (#65): add google font dependencies

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -14,8 +14,8 @@ const sidebar = {
         '/guide/list',
         '/guide/events',
         '/guide/forms',
-        '/guide/component-basics',
-      ],
+        '/guide/component-basics'
+      ]
     },
     {
       title: 'Components In-Depth',
@@ -26,8 +26,8 @@ const sidebar = {
         '/guide/component-custom-events',
         '/guide/component-slots',
         '/guide/component-provide-inject',
-        '/guide/component-dynamic-async',
-      ],
+        '/guide/component-dynamic-async'
+      ]
     },
     {
       title: 'Reusability & Composition',
@@ -37,14 +37,14 @@ const sidebar = {
     {
       title: 'Migration to Vue 3',
       collapsable: true,
-      children: ['migration'],
+      children: ['migration']
     },
     {
       title: 'Contribute to the Docs',
       collapsable: true,
-      children: ['writing-guide'],
-    },
-  ],
+      children: ['writing-guide']
+    }
+  ]
 }
 
 module.exports = {
@@ -55,10 +55,18 @@ module.exports = {
       'link',
       {
         href:
-          'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-        rel: 'stylesheet',
-      },
+          'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600|Roboto Mono&amp;display=swap',
+        rel: 'stylesheet'
+      }
     ],
+    [
+      ('link',
+      {
+        href:
+          'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+        rel: 'stylesheet'
+      })
+    ]
   ],
   extend: '@vuepress/theme-default',
   themeConfig: {
@@ -69,8 +77,8 @@ module.exports = {
         items: [
           { text: 'Guide', link: '/guide/introduction' },
           { text: 'Style Guide', link: '/style-guide/' },
-          { text: 'Tooling', link: '/tooling/' },
-        ],
+          { text: 'Tooling', link: '/tooling/' }
+        ]
       },
       { text: 'API Reference', link: '/api/' },
       {
@@ -78,8 +86,8 @@ module.exports = {
         ariaLabel: 'Examples Menu',
         items: [
           { text: 'Examples', link: '/examples/' },
-          { text: 'Cookbook', link: '/cookbook/' },
-        ],
+          { text: 'Cookbook', link: '/cookbook/' }
+        ]
       },
       {
         text: 'Community',
@@ -87,16 +95,16 @@ module.exports = {
         items: [
           { text: 'Team', link: '/community/team/' },
           { text: 'Partners', link: '/community/partners/' },
-          { text: 'Themes', link: '/community/themes/' },
-        ],
-      },
+          { text: 'Themes', link: '/community/themes/' }
+        ]
+      }
     ],
     sidebarDepth: 2,
     sidebar: {
       '/guide/': sidebar.guide,
-      '/community/': sidebar.guide,
+      '/community/': sidebar.guide
     },
-    smoothScroll: false,
+    smoothScroll: false
   },
   plugins: {
     '@vuepress/pwa': {
@@ -104,17 +112,17 @@ module.exports = {
       updatePopup: {
         '/': {
           message: 'New content is available.',
-          buttonText: 'Refresh',
-        },
-      },
-    },
+          buttonText: 'Refresh'
+        }
+      }
+    }
   },
   markdown: {
     /** @param {import('markdown-it')} md */
-    extendMarkdown: (md) => {
+    extendMarkdown: md => {
       md.options.highlight = require('./markdown/highlight')(
         md.options.highlight
       )
-    },
-  },
+    }
+  }
 }
