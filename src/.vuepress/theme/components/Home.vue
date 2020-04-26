9a1385f9e581cf549afe0a8f0d41b71e94598d8a
An Phan <me@phanan.net>
2020-04-26 12:04:11 +0200

    chore: remove superfluous actionLink from Home

--- src/.vuepress/theme/components/Home.vue
+++ src/.vuepress/theme/components/Home.vue
@@ -113,13 +113,6 @@ export default {
   computed: {
     data () {
       return this.$page.frontmatter
-    },
-
-    actionLink () {
-      return {
-        link: this.data.actionLink,
-        text: this.data.actionText
-      }
     }
   },
 
