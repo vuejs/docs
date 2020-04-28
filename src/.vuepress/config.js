70210b34e3895f7313fe0301966f7cf356c7a3b0
Sarah Drasner <sarah.drasner@gmail.com>
2020-04-28 11:20:49 -0600

    Rough Draft of New Reactivity Section (#62)
    * add a bit about scoping down PRs into our writing guide
    
    * add reactivity into sidebar
    
    * add in first reactivity section
    
    * start to get in the next section
    
    * add in older section
    
    * add in codepen examples
    
    * embed the video
    
    * Fix typo, 'cell' vs 'cel'
    
    Co-Authored-By: Evan You <yyx990803@gmail.com>
    
    * Add space
    
    Co-Authored-By: Evan You <yyx990803@gmail.com>
    
    * Move proxies up a paragragh
    
    Co-Authored-By: Natalia Tepluhina <tarya.se@gmail.com>
    
    * Fix quotations
    
    Co-Authored-By: Natalia Tepluhina <tarya.se@gmail.com>
    
    * small tone updates and change detection page
    
    * move virtual dom to a new dedicated page
    
    * add new pages to the sidebar
    
    * add in more information and original vs proxied caveats
    
    * small copy adjustments: strikethrough and array method formatting
    
    * Add space
    
    Co-Authored-By: Ben Hong <ben@bencodezen.io>
    
    * Add space
    
    Co-Authored-By: Ben Hong <ben@bencodezen.io>
    
    * Add space
    
    Co-Authored-By: Ben Hong <ben@bencodezen.io>
    
    Co-authored-by: Evan You <yyx990803@gmail.com>
    Co-authored-by: Natalia Tepluhina <tarya.se@gmail.com>
    Co-authored-by: Ben Hong <ben@bencodezen.io>

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -14,8 +14,8 @@ const sidebar = {
         '/guide/list',
         '/guide/events',
         '/guide/forms',
-        '/guide/component-basics',
-      ],
+        '/guide/component-basics'
+      ]
     },
     {
       title: 'Components In-Depth',
@@ -25,9 +25,18 @@ const sidebar = {
         '/guide/component-props',
         '/guide/component-custom-events',
         '/guide/component-slots',
-        '/guide/component-provide-inject',
+        '/guide/component-provide-inject'
+      ]
+    },
+    {
+      title: 'Internals',
+      collapsable: false,
+      children: [
+        '/guide/reactivity',
         '/guide/component-dynamic-async',
-      ],
+        '/guide/optimizations',
+        '/guide/change-detection'
+      ]
     },
     {
       title: 'Reusability & Composition',
@@ -37,14 +46,14 @@ const sidebar = {
     {
       title: 'Migration to Vue 3',
       collapsable: true,
-      children: ['migration'],
+      children: ['migration']
     },
     {
       title: 'Contribute to the Docs',
       collapsable: true,
-      children: ['writing-guide'],
-    },
-  ],
+      children: ['writing-guide']
+    }
+  ]
 }
 
 module.exports = {
@@ -80,8 +89,8 @@ module.exports = {
         items: [
           { text: 'Guide', link: '/guide/introduction' },
           { text: 'Style Guide', link: '/style-guide/' },
-          { text: 'Tooling', link: '/tooling/' },
-        ],
+          { text: 'Tooling', link: '/tooling/' }
+        ]
       },
       { text: 'API Reference', link: '/api/' },
       {
@@ -89,8 +98,8 @@ module.exports = {
         ariaLabel: 'Examples Menu',
         items: [
           { text: 'Examples', link: '/examples/' },
-          { text: 'Cookbook', link: '/cookbook/' },
-        ],
+          { text: 'Cookbook', link: '/cookbook/' }
+        ]
       },
       {
         text: 'Community',
@@ -99,16 +108,16 @@ module.exports = {
           { text: 'Team', link: '/community/team/' },
           { text: 'Partners', link: '/community/partners/' },
           { text: 'Join', link: '/community/join/' },
-          { text: 'Themes', link: '/community/themes/' },
-        ],
-      },
+          { text: 'Themes', link: '/community/themes/' }
+        ]
+      }
     ],
     sidebarDepth: 2,
     sidebar: {
       '/guide/': sidebar.guide,
-      '/community/': sidebar.guide,
+      '/community/': sidebar.guide
     },
-    smoothScroll: false,
+    smoothScroll: false
   },
   plugins: {
     '@vuepress/pwa': {
@@ -116,17 +125,17 @@ module.exports = {
       updatePopup: {
         '/': {
           message: 'New content is available.',
-          buttonText: 'Refresh',
-        },
-      },
-    },
+          buttonText: 'Refresh'
+        }
+      }
+    }
   },
   markdown: {
     /** @param {import('markdown-it')} md */
-    extendMarkdown: (md) => {
+    extendMarkdown: md => {
       md.options.highlight = require('./markdown/highlight')(
         md.options.highlight
       )
-    },
-  },
+    }
+  }
 }
