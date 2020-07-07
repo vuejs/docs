acc5af61ae893f958850365384dd81e64ea84289
Ben Hong <ben@bencodezen.io>
2020-07-06 23:39:07 -0400

    docs (#137): add async component to migration guide

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -75,7 +75,7 @@ const sidebar = {
     {
       title: 'Migration to Vue 3',
       collapsable: true,
-      children: ['migration']
+      children: ['migration', 'migration/async-components']
     },
     {
       title: 'Contribute to the Docs',
