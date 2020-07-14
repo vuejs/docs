eabae3bf6f934a2c0ff2518257378e593219082b
Ben Hong <ben@bencodezen.io>
2020-07-14 10:44:37 -0400

    feature (#170): add temporary beta banner

---
+++ src/.vuepress/components/BetaBanner.vue
@@ -0,0 +1,11 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <aside class="beta-banner">
+    <p>⚠️ Beta Version: Docs are still in progress and may change. ⚠️</p>
+  </aside>
+</template>
+
+<style></style>
