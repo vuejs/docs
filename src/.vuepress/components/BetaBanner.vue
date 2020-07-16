2d04b0b62c38801bcb21fd36fe2474ce1f764be3
Ben Hong <ben@bencodezen.io>
2020-07-16 16:10:54 -0400

    Fix/194 banner on mobile (#198)
    * fix (#194): update mobile banner styles
    
    * feature: add responsive mixin for future styles

--- src/.vuepress/components/BetaBanner.vue
+++ src/.vuepress/components/BetaBanner.vue
@@ -6,9 +6,32 @@ export default {}
   <aside class="beta-banner">
     <p>
       ⚠️ Beta Version: Docs are still under development and are subject to
-      change. ⚠️
+      change.
     </p>
   </aside>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+.beta-banner {
+  position: fixed;
+  z-index: 20;
+  top: 3.6rem;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fffedb;
+  box-sizing: border-box;
+  border-bottom: 1px solid #eaecef;
+  font-weight: bold;
+  text-align: center;
+  padding: 10px 20px;
+  box-sizing: border-box;
+
+  p {
+    margin: 0;
+    line-height: 1.4;
+  }
+}
+</style>
