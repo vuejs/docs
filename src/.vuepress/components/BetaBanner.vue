0a4b35fc0694e99046256268d5494bceaa098fb0
Ben Hong <ben@bencodezen.io>
2020-07-16 21:29:34 -0400

    feature (#201): Update BetaBanner to be thinner and less intrusive to the reading experience

--- src/.vuepress/components/BetaBanner.vue
+++ src/.vuepress/components/BetaBanner.vue
@@ -5,17 +5,18 @@ export default {}
 <template>
   <aside class="beta-banner">
     <p>
-      ⚠️ Beta Version: Docs are still under development and are subject to
-      change.
+      ⚠️ Beta Version: Docs are in development and subject to change.
     </p>
   </aside>
 </template>
 
 <style lang="scss" scoped>
+@import '../theme/styles/mixins.scss';
+
 .beta-banner {
   position: fixed;
   z-index: 20;
-  top: 3.6rem;
+  top: 0;
   left: 0;
   right: 0;
   display: flex;
@@ -24,7 +25,6 @@ export default {}
   background-color: #fffedb;
   box-sizing: border-box;
   border-bottom: 1px solid #eaecef;
-  font-weight: bold;
   text-align: center;
   padding: 10px 20px;
   box-sizing: border-box;
@@ -32,6 +32,15 @@ export default {}
   p {
     margin: 0;
     line-height: 1.4;
+    font-size: 0.7rem;
+    font-weight: 600;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    @include breakpoint(700px) {
+      font-size: 0.8rem;
+    }
   }
 }
 </style>
