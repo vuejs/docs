be972f06e57ab665fe50f951a5753d8899226e68
Phan An <me@phanan.net>
2020-07-19 19:57:43 +0200

    chore: allow discarding beta banner (#251)
    * chore: allow discarding beta banner
    
    * fix: ensure localStorage only accessed at client

--- src/.vuepress/components/BetaBanner.vue
+++ src/.vuepress/components/BetaBanner.vue
@@ -1,8 +1,40 @@
 <template>
-  <aside class="beta-banner">
-    <p>⚠️ Beta Version: Docs are in development and subject to change.</p>
-  </aside>
+  <ClientOnly>
+    <aside class="beta-banner" v-if="shouldShow">
+      <p>
+        <span class="hide-sm">⚠️ Beta Version:</span>
+        Docs
+        <span class="hide-sm">are</span> in development and subject to change.
+      </p>
+      <button ref="closeButton" @click="close">Close</button>
+    </aside>
+  </ClientOnly>
 </template>
+
+<script>
+export default {
+  computed: {
+    shouldShow() {
+      if (typeof window === 'undefined') return true
+      return window.localStorage.getItem('beta-banner-discarded') === null
+    }
+  },
+
+  methods: {
+    close() {
+      window.localStorage.setItem('beta-banner-discarded', true)
+      this.$el.remove()
+      document.querySelector('html').classList.remove('with-beta-banner')
+    }
+  },
+
+  beforeMount() {
+    if (this.shouldShow) {
+      document.querySelector('html').classList.add('with-beta-banner')
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 @import '../theme/styles/mixins.scss';
@@ -34,6 +66,36 @@
 
     @include breakpoint(700px) {
       font-size: 0.8rem;
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 1.5rem;
+    border: #f60 1px solid;
+    background: #f60;
+    color: #fff;
+    -webkit-appearance: none;
+    border-radius: 99px;
+    cursor: pointer;
+    line-height: 1.5;
+    padding: 0 0.5rem;
+  }
+
+  @media only screen and (max-device-width: 541px) {
+    .hide-sm {
+      display: none;
+    }
+
+    p {
+      flex: 1;
+      font-size: 0.8rem;
+      font-weight: normal;
+    }
+
+    button {
+      position: relative;
+      right: 0;
     }
   }
 }
