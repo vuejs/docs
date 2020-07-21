ed200f82f2cd26e5beacaf38962d378e98ce13b3
Marina Mosti <marina@mosti.com.mx>
2020-07-21 17:06:55 +0100

    normalize hash links

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -257,8 +257,8 @@ module.exports = {
         text: 'Support Vue',
         link: '/support-vuejs/',
         items: [
-          { text: 'One-time Donations', link: '/support-vuejs/#One-time-Donations' },
-          { text: 'Recurring Pledges', link: '/support-vuejs/#Recurring-pledges' },
+          { text: 'One-time Donations', link: '/support-vuejs/#one-time-donations' },
+          { text: 'Recurring Pledges', link: '/support-vuejs/#recurring-pledges' },
           { text: 'T-Shirt Shop', link: 'https://vue.threadless.com/' }
         ]
       }
