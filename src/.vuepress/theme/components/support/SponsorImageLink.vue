8a838498e182570819728a163b0ebcec74791ece
Marina Mosti <marina@mosti.com.mx>
2020-07-21 17:06:38 +0100

    create supportimagelink component

---
+++ src/.vuepress/theme/components/support/SponsorImageLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <a
+    :href="sponsor.url"
+    target="_blank"
+    rel="noopener"
+  >
+    <img
+      :src="`/images/sponsors/${sponsor.img}`"
+      :style="imageStyles"
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    sponsor: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    imageStyles () {
+      const styles = []
+      if (this.sponsor.big_width) styles.push(`width:${sponsor.big_width};`)
+      if (this.sponsor.big_height) styles.push(`height:${sponsor.big_height};`)
+      return styles
+    }
+  }
+}
+</script>
