fb7ab35c7d10639189e6cddeabe24caea716247d
Marina Mosti <marina@mosti.com.mx>
2020-07-22 18:55:49 +0100

    relocate components and prep for MD

---
+++ src/.vuepress/components/support/OpenCollectiveGroup.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sponsor-group open-collective-sponsors sponsor-section">
+    <a
+      v-for="i in 10"
+      :key="i"
+      :href="`${url}/${i}/website`"
+      target="_blank"
+      rel="noopener"
+    >
+      <img
+        :src="`${url}/${i}/avatar.svg`"
+      >
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { group: { type: String, requird: true } },
+  computed: {
+    url () {
+      return `https://opencollective.com/vuejs/tiers/${this.group}-sponsors`
+    }
+  }
+}
+</script>
