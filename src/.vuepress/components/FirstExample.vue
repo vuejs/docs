1606928b8ca39f99381039f0f4bdd355f12ea8f4
NataliaTepluhina <tarya.se@gmail.com>
2020-07-26 17:49:50 +0300

    chore: replaced first example in Introduction

---
+++ src/.vuepress/components/FirstExample.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">Counter: {{ counter }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
