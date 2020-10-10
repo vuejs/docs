9c076f79a3d7ee95e7ab5871faaf706944369e94
Serhii Matrunchyk <serhii@matrunchyk.com>
2020-10-10 16:46:00 +0300

    Update index

--- src/.vuepress/theme/components/sponsors/SpecialSponsors.vue
+++ src/.vuepress/theme/components/sponsors/SpecialSponsors.vue
@@ -26,7 +26,7 @@ export default {
 
   computed: {
     heading () {
-      return `Special Sponsor${this.sponsors.length === 1 ? '' : 's'}`
+      return this.sponsors.length === 1 ? 'Спеціальний спонсор' : 'Спеціальні спонсори'
     }
   }
 }
