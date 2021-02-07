cb38a4945b1f0e00709d850bc8d4ff55783a8059
Andrei <12499409+andreiTn@users.noreply.github.com>
2021-02-07 18:52:47 +0000

    docs: update vue test utils link (#850)

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -363,7 +363,7 @@ module.exports = {
               {
                 text: 'Vue Test Utils',
                 link:
-                  'https://vuejs.github.io/vue-test-utils-next-docs/guide/introduction.html'
+                  'https://next.vue-test-utils.vuejs.org/v2/guide/introduction.html'
               },
               {
                 text: 'Devtools',
