e95dc4c1ebfd775c142114eea117d38b7d208b66
Mohamed Donzo <69730567+demahom18@users.noreply.github.com>
2021-04-04 18:56:47 -0400

    French translation (#955)
    * French translation
    
    Add french translation in the community translation table
    
    * Update translations-data.js

--- src/.vuepress/components/guide/contributing/translations-data.js
+++ src/.vuepress/components/guide/contributing/translations-data.js
@@ -12,6 +12,7 @@ export const labels = {
 // You may need to clear your sessionStorage when adding a new item to this list
 export const repos = [
   { lang: 'en-us', owner: 'vuejs', repo: 'docs-next', branch: 'master', url: 'https://v3.vuejs.org/' },
+  { lang: 'fr', owner: 'demahom18', repo: 'docs-next', branch: 'master', url: 'https://vue3-fr.netlify.app' },
   { lang: 'id', owner: 'vuejs-id', repo: 'docs-next', branch: 'indonesian' },
   { lang: 'ja', owner: 'vuejs-jp', repo: 'ja.vuejs.org', branch: 'lang-ja', url: 'https://v3.ja.vuejs.org/' },
   { lang: 'ko', owner: 'vuejs-kr', repo: 'docs-next', branch: 'rootKoKr', url: 'https://v3.ko.vuejs.org/'  },
