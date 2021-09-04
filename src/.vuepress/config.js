dbdcb7e09f51e425974e72fadce83e17e7b80299
Anik Das <anikdas0811@gmail.com>
2021-09-04 11:02:56 +0530

    fix: firefox mobile view fixed
    added viewport meta tag in vitepress config
    
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -296,6 +296,7 @@ module.exports = {
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
     [
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
