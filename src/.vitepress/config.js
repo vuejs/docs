c466fa1619f83be6bc86ee13d890a1eadd7d73c1
Kia King Ishii <kia.king.08@gmail.com>
2021-09-16 20:48:58 +0900

    feat: configure global footer

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -534,6 +534,14 @@ module.exports = {
     ],
 
     nav,
-    sidebar
+    sidebar,
+
+    footer: {
+      license: {
+        text: 'MIT License',
+        link: 'https://opensource.org/licenses/MIT'
+      },
+      copyright: 'Copyright © 2014-2021 Evan You'
+    }
   }
 }
