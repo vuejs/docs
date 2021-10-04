6581bbfadbf4e41849e8dde8f80fc735d9ed0117
Kia King Ishii <kia.king.08@gmail.com>
2021-10-04 23:05:23 +0900

    feat: add newsletter section (#1246)

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -2,13 +2,15 @@ import { h } from 'vue'
 import { VPTheme, VTBadge } from '@vue/theme'
 import PreferenceSwitch from './components/PreferenceSwitch.vue'
 import { preferComposition, preferSFC } from './components/preferences'
+import NewsletterSection from './components/NewsletterSection.vue'
 import './styles/inline-demo.css'
 import './styles/options-boxes.css'
 
 export default Object.assign({}, VPTheme, {
   Layout: () => {
     return h(VPTheme.Layout, null, {
-      'sidebar-top': () => h(PreferenceSwitch)
+      'sidebar-top': () => h(PreferenceSwitch),
+      'footer-before': () => h(NewsletterSection)
     })
   },
   enhanceApp({ app }) {
