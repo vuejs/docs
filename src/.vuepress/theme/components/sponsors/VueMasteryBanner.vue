83a0149da2d87bcc1ed15af06dbded714c4e2aab
Pierre Schweiger <schweiger.pierre@gmail.com>
2021-12-07 17:19:36 +0700

    Update promotion banner url

--- src/.vuepress/theme/components/sponsors/VueMasteryBanner.vue
+++ src/.vuepress/theme/components/sponsors/VueMasteryBanner.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="vm-banner" class="vuemastery-banner hide" role="banner">
-  <a href="https://www.vuemastery.com/free-weekend-december-2021" target="_blank">
+  <a href="https://www.vuemastery.com/free-weekend" target="_blank">
     <div class="vuemastery-banner--wrapper">
       <img class="vuemastery-banner--logo" src="/images/vuemastery/logo-vuemastery.svg" alt="vuemastery" />
       <p>Access the ultimate collection of Vue courses free. <span>Dec 17-20</span></p>
