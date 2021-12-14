28f7dce2ec35bbdea6876eb86d28e13b928bfce2
Evan You <yyx990803@gmail.com>
2021-12-15 00:01:26 +0800

    clean up types

--- src/.vitepress/env.d.ts
+++ src/.vitepress/env.d.ts
@@ -1,2 +1,14 @@
-/// <reference types="vite/client" />
+/// <reference types="vitepress/client" />
+
 declare module '*.vue' {}
+
+declare module '@vue/theme/config' {
+  import { UserConfig } from 'vitepress'
+  const config: () => Promise<UserConfig>
+  export default config
+}
+
+declare module '@vue/theme/highlight' {
+  const createHighlighter: () => Promise<(input: string) => string>
+  export default createHighlighter
+}
