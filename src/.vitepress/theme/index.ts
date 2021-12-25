ef65ab46a1233dc2fc25a5ccdfc8068312dcb7e4
Evan You <yyx990803@gmail.com>
2021-12-25 21:34:28 +0800

    minor type tweaks

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,4 +1,4 @@
-import { h } from 'vue'
+import { h, App } from 'vue'
 import { VPTheme, VTBadge } from '@vue/theme'
 import Banner from './components/Banner.vue'
 import PreferenceSwitch from './components/PreferenceSwitch.vue'
@@ -14,12 +14,13 @@ import './styles/options-boxes.css'
 
 export default Object.assign({}, VPTheme, {
   Layout: () => {
+    // @ts-ignore
     return h(VPTheme.Layout, null, {
       banner: () => h(Banner),
       'sidebar-top': () => h(PreferenceSwitch)
     })
   },
-  enhanceApp({ app }) {
+  enhanceApp({ app }: { app: App }) {
     app.component('Badge', VTBadge)
     app.provide('prefer-composition', preferComposition)
     app.provide('prefer-sfc', preferSFC)
