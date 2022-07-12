9bd07ac8a5ee6bc70affda9c1a3459167e7e8741
Evan You <yyx990803@gmail.com>
2022-07-12 10:32:46 +0800

    Revert "Add vuemastery banner (#1825)"
    This reverts commit d26b8a4d42c41d5dbe2089a8c57bf023ef5f6692.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,22 +2,19 @@ import './styles/index.css'
 import { h, App } from 'vue'
 import { VPTheme } from '@vue/theme'
 import PreferenceSwitch from './components/PreferenceSwitch.vue'
-import VueMasteryBanner from './components/VueMasteryBanner.vue'
 import {
   preferComposition,
   preferSFC,
   filterHeadersByPreference
 } from './components/preferences'
 import SponsorsAside from './components/SponsorsAside.vue'
+import VueSchoolLink from './components/VueSchoolLink.vue'
 import VueJobs from './components/VueJobs.vue'
 
 export default Object.assign({}, VPTheme, {
   Layout: () => {
     // @ts-ignore
     return h(VPTheme.Layout, null, {
-      banner: () => h('div', {}, [
-        h(VueMasteryBanner)
-      ]),
       'sidebar-top': () => h(PreferenceSwitch),
       'aside-mid': () => h(SponsorsAside),
       'aside-bottom': () => h(VueJobs)
@@ -27,5 +24,6 @@ export default Object.assign({}, VPTheme, {
     app.provide('prefer-composition', preferComposition)
     app.provide('prefer-sfc', preferSFC)
     app.provide('filter-headers', filterHeadersByPreference)
+    app.component('VueSchoolLink', VueSchoolLink)
   }
 })
