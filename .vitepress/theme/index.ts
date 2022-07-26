63065f816c6d42483ba96a7ea74b37bd1067d7b4
Evan You <yyx990803@gmail.com>
2022-07-26 10:32:18 +0800

    chore: remove VueMastery banner

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,7 +2,6 @@ import './styles/index.css'
 import { h, App } from 'vue'
 import { VPTheme } from '@vue/theme'
 import PreferenceSwitch from './components/PreferenceSwitch.vue'
-import VueMasteryBanner from './components/VueMasteryBanner.vue'
 import {
   preferComposition,
   preferSFC,
@@ -11,14 +10,13 @@ import {
 import SponsorsAside from './components/SponsorsAside.vue'
 import VueJobs from './components/VueJobs.vue'
 import VueSchoolLink from './components/VueSchoolLink.vue'
+// import Banner from './components/Banner.vue'
 
 export default Object.assign({}, VPTheme, {
   Layout: () => {
     // @ts-ignore
     return h(VPTheme.Layout, null, {
-      banner: () => h('div', {}, [
-        h(VueMasteryBanner)
-      ]),
+      // banner: () => h(Banner),
       'sidebar-top': () => h(PreferenceSwitch),
       'aside-mid': () => h(SponsorsAside),
       'aside-bottom': () => h(VueJobs)
