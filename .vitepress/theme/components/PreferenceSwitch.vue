e7de31094eddc40987ce1685baa0022276a68021
Sepush <sepush@outlook.com>
2022-08-08 18:03:37 +0800

    fix(PreferenceSwitch): switch-link style overflow (#1828)
    * fix(PreferenceSwitch): switch-link style overflow
    
    Signed-off-by: Sepush <sepush@outlook.com>
    
    * Update .vitepress/theme/components/PreferenceSwitch.vue
    
    Co-authored-by: skirtle <65301168+skirtles-code@users.noreply.github.com>
    
    Co-authored-by: skirtle <65301168+skirtles-code@users.noreply.github.com>

--- .vitepress/theme/components/PreferenceSwitch.vue
+++ .vitepress/theme/components/PreferenceSwitch.vue
@@ -204,7 +204,15 @@ function useToggleFn(
 @media (max-width: 1439px) {
   #preference-switches {
     font-size: 11px;
-    padding: 8px 12px;
+    padding: 8px 4px;
+  }
+  
+  .vt-switch {
+    margin: auto;
+  }
+  
+  .switch-link {
+    margin-left: auto;
   }
   .switch-container label:first-child {
     width: 46px;
