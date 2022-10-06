c2c67c477a6f4be5a57b8273af5dd42cd4807649
Nico Devs <nico1485@gmail.com>
2022-10-06 09:48:09 -0300

    Remove Vue School top banner

--- .vitepress/inlined-scripts/restorePreference.js
+++ .vitepress/inlined-scripts/restorePreference.js
@@ -8,6 +8,6 @@
   restore('vue-docs-prefer-composition', 'prefer-composition')
   restore('vue-docs-prefer-sfc', 'prefer-sfc', true)
 
-  window.__VUE_BANNER_ID__ = 'VS-FW'
-  restore(`vue-docs-banner-${__VUE_BANNER_ID__}`, 'banner-dismissed')
+  // window.__VUE_BANNER_ID__ = ''
+  // restore(`vue-docs-banner-${__VUE_BANNER_ID__}`, 'banner-dismissed')
 })()
