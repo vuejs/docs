789d11518f3e4e1fca385f4293db932d41647730
Alex Sokolov <4497128+Alex-Sokolov@users.noreply.github.com>
2022-10-16 14:22:28 +0300

    docs(ru): tutorial / commons translated

--- src/tutorial/TutorialRepl.vue
+++ src/tutorial/TutorialRepl.vue
@@ -121,16 +121,16 @@ updateExample()
       <div class="vt-doc" v-html="currentDescription"></div>
       <div class="hint" v-if="data[currentStep]?._hint">
         <button @click="toggleResult">
-          {{ showingHint ? 'Reset' : 'Show me!' }}
+          {{ showingHint ? 'Сбросить назад' : 'Покажите как!' }}
         </button>
       </div>
       <footer>
         <a v-if="prevStep" :href="`#${prevStep}`"
           ><VTIconChevronLeft class="vt-link-icon" style="margin: 0" />
-          Prev</a
+          Назад</a
         >
         <a class="next-step" v-if="nextStep" :href="`#${nextStep}`"
-          >Next <VTIconChevronRight class="vt-link-icon"
+          >Дальше <VTIconChevronRight class="vt-link-icon"
         /></a>
       </footer>
     </article>
