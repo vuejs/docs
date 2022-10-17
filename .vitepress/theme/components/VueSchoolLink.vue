f0bd4c01223611422f306dd102495c8d24a59c45
Alex Sokolov <4497128+Alex-Sokolov@users.noreply.github.com>
2022-10-17 23:43:53 +0300

    docs(ru): translated common text in VueSchoolLink

--- .vitepress/theme/components/VueSchoolLink.vue
+++ .vitepress/theme/components/VueSchoolLink.vue
@@ -6,7 +6,7 @@
       rel="sponsored noopener"
       :title="title"
     >
-      <slot>Watch a free video lesson on Vue School</slot>
+      <slot>Посмотрите бесплатный видеоурок на Vue School (на англ.)</slot>
     </a>
   </div>
 </template>
