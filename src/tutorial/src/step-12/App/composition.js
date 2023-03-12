3f9afb167395ffef6c5b6c586d55f5e88cffb150
olesyakogivchak <olesya.kohivchak@evry.com>
2023-03-12 19:03:21 +0100

    Переклад: Посібник, крок 12

--- src/tutorial/src/step-12/App/composition.js
+++ src/tutorial/src/step-12/App/composition.js
@@ -6,7 +6,7 @@ export default {
     ChildComp
   },
   setup() {
-    const greeting = ref('Hello from parent')
+    const greeting = ref('Привіт від батьківського компонента!')
 
     return {
       greeting
--- src/tutorial/src/step-12/App/options.js
+++ src/tutorial/src/step-12/App/options.js
@@ -6,7 +6,7 @@ export default {
   },
   data() {
     return {
-      greeting: 'Hello from parent'
+      greeting: 'Привіт від батьківського компонента!'
     }
   }
 }
