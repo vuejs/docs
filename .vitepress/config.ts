9dc8f2f0bbb5c479916c802708f87e7fb0737688
Adam Williams <lol768@users.noreply.github.com>
2023-05-05 22:07:34 +0100

    Improve developer experience

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,7 +4,6 @@ import { defineConfigWithTheme } from 'vitepress'
 import type { Config as ThemeConfig } from '@vue/theme'
 import baseConfig from '@vue/theme/config'
 import { headerPlugin } from './headerMdPlugin'
-import { textAdPlugin } from './textAdMdPlugin'
 
 const nav: ThemeConfig['nav'] = [
   {
@@ -678,7 +677,7 @@ export default defineConfigWithTheme<ThemeConfig>({
 
   markdown: {
     config(md) {
-      md.use(headerPlugin).use(textAdPlugin)
+      md.use(headerPlugin)
     }
   },
 
