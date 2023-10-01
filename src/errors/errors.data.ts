27082278739eef50a30651a152cb1aa2c4ee3067
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-10-01 23:13:48 +0800

    refactor: fix prod version

--- src/errors/errors.data.ts
+++ src/errors/errors.data.ts
@@ -1,7 +1,7 @@
 import { defineLoader } from 'vitepress'
 import { errorMessages } from '@vue/compiler-core'
 // @ts-expect-error internal api
-import { ErrorTypeStrings } from '@vue/runtime-core'
+import { ErrorTypeStrings } from '@vue/runtime-core/dist/runtime-core.cjs.js'
 
 function filterEmptyMsg(data: Record<number, string>) {
   return Object.fromEntries(Object.entries(data).filter(([_, msg]) => msg))
