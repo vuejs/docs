a6fa35fdfcca4f4813aa0af5c011311339b960cb
Pax <phoenix081@gmail.com>
2023-10-12 04:39:00 +0200

    Italian translation ready to launch (#2533)
    * Adding Italian urls
    
    * order fix

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -652,6 +652,11 @@ export default defineConfigWithTheme<ThemeConfig>({
         repo: 'https://github.com/vuejs-translations/docs-bn'
       },
       {
+        link: 'https://it.vuejs.org',
+        text: 'Italiano',
+        repo: 'https://github.com/vuejs-translations/docs-it'
+      },
+      {
         link: '/translations/',
         text: 'Help Us Translate!',
         isTranslationsDesc: true
