a994c85625c3cd1329e9b7b45efec9d8262018ad
Israel Ugbodaga <israelugbodaga@gmail.com>
2023-11-03 13:31:08 +0100

    feat: add vuemastery free-weekend banner

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -9,6 +9,7 @@ import {
 } from './components/preferences'
 import SponsorsAside from './components/SponsorsAside.vue'
 import VueSchoolLink from './components/VueSchoolLink.vue'
+import VueMasteryBanner from './components/VueMasteryBanner.vue'
 // import Banner from './components/Banner.vue'
 // import TextAd from './components/TextAd.vue'
 
@@ -16,7 +17,7 @@ export default Object.assign({}, VPTheme, {
   Layout: () => {
     // @ts-ignore
     return h(VPTheme.Layout, null, {
-      // banner: () => h(Banner),
+      banner: () => h(VueMasteryBanner),
       'sidebar-top': () => h(PreferenceSwitch),
       'aside-mid': () => h(SponsorsAside)
     })
