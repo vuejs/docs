e9cdaff35df612e1cb08d8da863a817534018f42
Israel <israelugbodaga@gmail.com>
2023-12-18 04:02:25 +0000

    feat: add vuemastery banner (#2591)

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -10,13 +10,15 @@ import {
 import SponsorsAside from './components/SponsorsAside.vue'
 import VueSchoolLink from './components/VueSchoolLink.vue'
 import Banner from './components/Banner.vue'
+import VueMasteryBanner from './components/VueMasteryBanner.vue'
 // import TextAd from './components/TextAd.vue'
 
 export default Object.assign({}, VPTheme, {
   Layout: () => {
     // @ts-ignore
     return h(VPTheme.Layout, null, {
-      banner: () => h(Banner),
+      // banner: () => h(Banner),
+      banner: () => h(VueMasteryBanner),
       'sidebar-top': () => h(PreferenceSwitch),
       'aside-mid': () => h(SponsorsAside)
     })
