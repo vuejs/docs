e77511d966f350191978450fadd83c59558a0fac
Alois Sečkár <ellrohir@seznam.cz>
2024-03-29 19:58:39 +0100

    feat: czech translation (#2772)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -675,6 +675,11 @@ export default defineConfigWithTheme<ThemeConfig>({
         repo: 'https://github.com/vuejs-translations/docs-fa'
       },
       {
+        link: 'https://cs.vuejs.org',
+        text: 'Čeština',
+        repo: 'https://github.com/vuejs-translations/docs-cs'
+      },
+      {
         link: '/translations/',
         text: 'Help Us Translate!',
         isTranslationsDesc: true
