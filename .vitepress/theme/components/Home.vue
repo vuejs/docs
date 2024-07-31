1bbec6d11142b6cd6600d11b0a4e084f610a4994
patak <583075+patak-dev@users.noreply.github.com>
2024-07-31 12:04:02 +0200

    chore: diff

--- .vitepress/theme/components/Home.vue
+++ .vitepress/theme/components/Home.vue
@@ -40,7 +40,7 @@ onMounted(load)
       <a class="setup" href="/guide/quick-start.html">Install</a>
       <a class="security" href="https://v2.vuejs.org/eol/" target="_blank">
         Get Security Updates for Vue 2
-        <svg
+        <svg 
           class="icon"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
