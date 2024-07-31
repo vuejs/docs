dc458fabf4a788aa3f2a763ef23cd1a384014318
patak <583075+patak-dev@users.noreply.github.com>
2024-07-31 12:04:44 +0200

    chore: diff

--- .vitepress/theme/components/Home.vue
+++ .vitepress/theme/components/Home.vue
@@ -158,7 +158,7 @@ html:not(.dark) .accent,
 }
 
 .actions .security {
-  background: linear-gradient(var(--vt-c-bg-mute), var(--vt-c-bg-mute)) padding-box,
+  background: linear-gradient(var(--vt-c-bg-mute), var(--vt-c-bg-mute)) padding-box, 
     linear-gradient(45deg, #42d392, #647eff) border-box;
   border: 2px solid transparent;
 }
