58c4b8992527c252b18b834e4712ebe682e43db2
Jilson Thomas <jilson@live.in>
2024-11-08 00:32:57 -0500

    Add VueToronto banner to the docs (#3099)
    feat(*): add vuetoronto banner

--- .vitepress/theme/components/Banner.vue
+++ .vitepress/theme/components/Banner.vue
@@ -22,16 +22,31 @@ function dismiss() {
 
 <template>
   <div class="banner" v-if="open">
-    <a target="_blank"></a>
+    <p class="vt-banner-text">
+      <span class="vt-text-primary">VueConf Toronto</span>
+      <span class="vt-tagline"> - Join the premier Vue.js conference</span>
+      | 18-20 Nov 2024 <span class="vt-place"> - Toronto, Canada</span>
+      <a
+        target="_blank"
+        class="vt-primary-action"
+        href="https://vuetoronto.com/?utm_source=vuejs&utm_content=top_banner"
+      >
+        Register
+      </a>
+    </p>
     <button @click="dismiss">
       <VTIconPlus class="close" />
     </button>
+    <p class="vt-banner-text vt-coupon">
+      <span class="vt-text-primary">Use code</span> VUEJS
+      <span class="vt-text-primary">to get 15% off</span>
+    </p>
   </div>
 </template>
 
 <style>
 html:not(.banner-dismissed) {
-  --vt-banner-height: 30px;
+  --vt-banner-height: 60px;
 }
 </style>
 
@@ -50,12 +65,10 @@ html:not(.banner-dismissed) {
   font-weight: 600;
   color: #fff;
   background-color: var(--vt-c-green);
-  background: linear-gradient(
-    90deg,
-    rgba(66, 184, 131, 1) 0%,
-    rgba(39, 179, 137, 1) 19%,
-    rgba(100, 126, 255, 1) 100%
-  );
+  background: #11252b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .banner-dismissed .banner {
@@ -70,7 +83,7 @@ button {
   position: absolute;
   right: 0;
   top: 0;
-  padding: 5px;
+  padding: 20px 10px;
 }
 
 .close {
@@ -79,10 +92,59 @@ button {
   fill: #fff;
   transform: rotate(45deg);
 }
-/*
-@media (max-width: 720px) {
-  a > span {
+
+.vt-banner-text {
+  color: #fff;
+  font-size: 16px;
+}
+
+.vt-text-primary {
+  color: #75c05e;
+}
+
+.vt-primary-action {
+  background: #75c05e;
+  color: #121c1a;
+  padding: 8px 15px;
+  border-radius: 5px;
+  font-size: 14px;
+  text-decoration: none;
+  margin: 0 20px;
+  font-weight: bold;
+}
+.vt-primary-action:hover {
+  text-decoration: none;
+  background: #5a9f45;
+}
+
+@media (max-width: 1280px) {
+  .banner .vt-banner-text {
+    font-size: 14px;
+  }
+  .vt-tagline {
     display: none;
   }
-} */
+}
+
+@media (max-width: 780px) {
+  .vt-tagline {
+    display: none;
+  }
+  .vt-coupon {
+    display: none;
+  }
+  .vt-primary-action {
+    margin: 0 10px;
+    padding: 7px 10px;
+  }
+  .vt-time-now {
+    display: none;
+  }
+}
+
+@media (max-width: 560px) {
+  .vt-place {
+    display: none;
+  }
+}
 </style>
