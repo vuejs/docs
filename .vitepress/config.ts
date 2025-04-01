c9186a6f0c029ac6a5edaf6216ca08ac5a069be7
Szymon Licau <78469990+szymonlicau@users.noreply.github.com>
2025-04-01 20:49:19 +0900

    feat: Mark Polish translation as complete (#3191)
    feat: Mark polish translation as complete

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -711,6 +711,11 @@ export default defineConfigWithTheme<ThemeConfig>({
         repo: 'https://github.com/vuejs-translations/docs-zh-hk'
       },
       {
+        link: 'https://pl.vuejs.org',
+        text: 'Polski',
+        repo: 'https://github.com/vuejs-translations/docs-pl',
+      },
+      {
         link: '/translations/',
         text: 'Help Us Translate!',
         isTranslationsDesc: true
