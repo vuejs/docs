fc99592e2edc536ce0e61e03afbd8fbb502b8661
Per Harald Borgen <perhborgen@gmail.com>
2025-05-29 09:04:47 +0200

    register ScrimbaLink component and change color on play button

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -10,6 +10,7 @@ import {
 } from './components/preferences'
 import SponsorsAside from './components/SponsorsAside.vue'
 import VueSchoolLink from './components/VueSchoolLink.vue'
+import ScrimbaLink from './components/ScrimbaLink.vue'
 import Banner from './components/Banner.vue'
 // import TextAd from './components/TextAd.vue'
 
@@ -28,6 +29,7 @@ export default Object.assign({}, VPTheme, {
     app.provide('prefer-sfc', preferSFC)
     app.provide('filter-headers', filterHeadersByPreference)
     app.component('VueSchoolLink', VueSchoolLink)
+    app.component('ScrimbaLink', ScrimbaLink)
     // app.component('TextAd', TextAd)
   }
 })
